e610c61410c80292327e92e6ce96e477daaaa668
Mohit Gayatri <mohitgayatri77@gmail.com>
2021-10-08 21:29:47 -0400

    Initial funtionality

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,27 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Malika Niazi on 10/8/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
